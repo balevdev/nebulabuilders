3d5b1e34a2c8ba19cafc307b7de4fccafbbd815e
balevdev <boyanbalevdev@gmail.com>
2024-10-13 20:40:46 +0300

    compressed

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import path from 'path'
+import { visualizer } from 'rollup-plugin-visualizer'
+import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
   root: path.join(__dirname, 'public'),
@@ -8,6 +10,12 @@ export default defineConfig({
     emptyOutDir: false,
     rollupOptions: {
       input: path.join(__dirname, 'src', 'main.ts'),
+      output: {
+        manualChunks: {
+          'fuse': ['fuse.js'],
+          'htmx': ['htmx.org'],
+        },
+      },
     },
   },
   plugins: [
@@ -18,5 +26,15 @@ export default defineConfig({
         this.addWatchFile(path.join(__dirname, 'content'))
       },
     },
+    viteCompression(),
+    visualizer({
+      filename: './public/stats.html',
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
   ],
+  optimizeDeps: {
+    include: ['fuse.js', 'htmx.org'],
+  },
 })
